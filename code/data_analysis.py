78f8f86834e613772e875a18f0c8de6a9d3ae000
Arghya Datta <arghya.dat@gmail.com>
2018-03-01 19:02:37 -0600

    updated data_analysis

--- code/data_analysis.py
+++ code/data_analysis.py
@@ -31,7 +31,28 @@ def crimes_per_day(df):
     plt.title('Distribution of Crimes per day', fontsize=16)
     plt.tick_params(labelsize=14)
     sns.distplot(df.resample('D').size(), bins=60);
-    
+
+def time_series_crimes_per_day(df):
+    df.index = pd.DatetimeIndex(df['DATE'])
+    # we need to create a range of possible values
+    data_daily = pd.DataFrame(df[df.DATE != "2011-06-15"].resample('D').size())
+    data_daily['MEAN'] = df[df.DATE != '2011-06-15'].resample('D').size().mean()
+    data_daily['STD'] = df[df.DATE != '2011-06-15'].resample('D').size().std()
+    upper_limit = data_daily['MEAN'] + 2 * data_daily['STD']
+    lower_limit = data_daily['MEAN'] - 2 * data_daily['STD']
+
+    plt.figure(figsize=(15,6))
+    df.resample('D').size().plot(label='Crimes per day')
+    upper_limit.plot(color='red', ls='--', linewidth=1.5, label='UCL')
+    lower_limit.plot(color='red', ls='--', linewidth=1.5, label='LCL')
+    data_daily['MEAN'].plot(color='red', linewidth=2, label='Average')
+    plt.title('Total crimes per day', fontsize=16)
+    plt.xlabel('Day')
+    plt.ylabel('Number of crimes')
+    plt.tick_params(labelsize=14)
+    plt.legend(prop={'size':16});
+
+   
 if __name__ == "__main__":
     df = pd.read_csv("raw_data/crime.csv")
     df['DATE'] = df.YEAR.astype('str').map(str) + '/' + df.MONTH.astype('str') + '/' +df.DAY.astype('str')
